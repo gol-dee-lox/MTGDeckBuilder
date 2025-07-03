a84ef8e6713df1b99be2cec7cf2b08dbe6f0ea2f
gol-dee-lox <randy@therealrandy.com>
2025-07-03 15:52:11 -0400

    Added explicit Redis Config

---
+++ src/main/java/com/goldeelox/mtg/MTGDeckBuilder/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.goldeelox.mtg.MTGDeckBuilder.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.*;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration standaloneConfig = new RedisStandaloneConfiguration(host, port);
+        standaloneConfig.setPassword(RedisPassword.of(password));
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+            .useSsl()  // ⚠️ Enables SSL/TLS
+            .build();
+
+        return new LettuceConnectionFactory(standaloneConfig, clientConfig);
+    }
+}
